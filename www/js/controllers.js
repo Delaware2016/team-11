c66c9b73906aaa25e9482a1b8e0fb1e9ffaae911
Stone Chen <thestonechen@gmail.com>
2016-10-08 01:01:18 -0400

    donate controlelr

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -47,28 +47,23 @@ angular.module('app.controllers', [])
   }
 ])
 
-.controller('donateCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-  // You can include any angular dependencies as parameters for this function
-  // TIP: Access Route Parameters for your page via $stateParams.parameterName
-  function($scope, $stateParams) {
-    $scope.items = []
+.controller('donateCtrl', ['$scope', '$stateParams', '$http',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $http) {
+  $scope.items = []
     $http.get('data/donate_data.json').success(function(data) {
       for (var i in data.donationcharities) {
         var item = {}
         item.values = data.donationcharities[i]
         item.str = "";
-        if (item.values.donate) {
-          item.str = item.values.name + " has donated " + item.values.value + " dollars to " + item.values.event_Name
-        } else {
-          item.str = item.values.name + " has volunteered for " + item.values.value + " hours at " + item.values.event_Name
-        }
+        item.str = "item.values.charityName"
         console.log(item)
         $scope.items.push(item)
       }
     });
-
-  }
-])
+    
+}])
 
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
   // You can include any angular dependencies as parameters for this function
