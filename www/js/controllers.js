118949833edff928c7e1406f805824032cb3c868
Stone Chen <thestonechen@gmail.com>
2016-10-07 23:59:43 -0400

    first commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,28 @@
 angular.module('app.controllers', [])
-  
-.controller('newsfeedCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
 
+.controller('newsfeedCtrl', ['$scope', '$stateParams', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  // You can include any angular dependencies as parameters for this function
+  // TIP: Access Route Parameters for your page via $stateParams.parameterName
+  function($scope, $stateParams, $http) {
+    $scope.items = []
+    $http.get('data/newsfeed_data.json').success(function(data) {
+      for (var i in data.array) {
+        var item = {}
+        item.values = data.array[i]
+        item.str = "";
+        if(item.values.donate) {
+          item.str = item.values.name + " has donated " + item.values.value + " dollars to " + item.values.event_Name
+        }
+        else {
+          item.str = item.values.name + " has volunteered for " + item.values.value + " hours at " + item.values.event_Name
+        }
+        console.log(item)
+        $scope.items.push(item)
+      }
+    });
+  }
+])
 
-}])
-   
 .controller('volunteerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -15,15 +30,30 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('donateCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-	
+	$scope.items = []
+    $http.get('data/donate_data.json').success(function(data) {
+      for (var i in data.donationcharities) {
+        var item = {}
+        item.values = data.donationcharities[i]
+        item.str = "";
+        if(item.values.donate) {
+          item.str = item.values.name + " has donated " + item.values.value + " dollars to " + item.values.event_Name
+        }
+        else {
+          item.str = item.values.name + " has volunteered for " + item.values.value + " hours at " + item.values.event_Name
+        }
+        console.log(item)
+        $scope.items.push(item)
+      }
+    });
 
 }])
-      
+
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -31,7 +61,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('aboutCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -39,7 +69,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('contactInfoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -47,7 +77,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -55,7 +85,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -63,7 +93,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('confirmDonationCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -71,4 +101,3 @@ function ($scope, $stateParams) {
 
 
 }])
- 
